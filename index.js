acbc759e205c49aba1bc99b3dbbf4b0d3415461e
patrickm4 <patrickmoreno444@gmail.com>
2023-07-10 23:18:09 -0700

    main feat: initial testing setup for trading api with authtokens

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import eBayApi from 'ebay-api';
-import config from 'config.js';
+import config from './config.js';
 
 const app = express()
 const port = 3030
@@ -11,7 +11,47 @@ const eBay = new eBayApi({
 	sandbox: true,
 	siteId: eBayApi.SiteId.EBAY_ENCA,
 	devId: config.devId,
+	authToken: config.authToken.testuser_jimtest
 });  
+
+const itemData = {
+	Item: {
+	  Title: 'Example Item',
+	  Description: {
+		__cdata: '<div>This is an example item for demonstration purposes.</div>'
+	  },
+	  StartPrice: '10.00',
+	  CategoryMappingAllowed: true,
+	  ConditionID: 1000, // Condition ID for new items
+	  Country: 'US',
+	  Currency: 'USD',
+	  ListingDuration: 'Days_7',
+	  Location: 'San Jose, CA',
+	  PaymentMethods: ['PayPal'],
+	  PayPalEmailAddress: 'your-paypal-email@example.com',
+	  PictureDetails: {
+		PictureURL: ['https://example.com/item-picture.jpg']
+	  },
+	  Quantity: 1,
+	  ReturnPolicy: {
+		ReturnsAcceptedOption: 'ReturnsAccepted',
+		RefundOption: 'MoneyBack',
+		ReturnsWithinOption: 'Days_30',
+		Description: 'If you are not satisfied with the item, you can return it within 30 days for a refund.'
+	  },
+	  ShippingDetails: {
+		ShippingServiceOptions: [
+		  {
+			ShippingServicePriority: 1,
+			ShippingService: 'USPSFirstClass',
+			ShippingServiceCost: '0.00'
+		  }
+		],
+		ShippingType: 'Flat'
+	  }
+	}
+  };
+  
 
 // Middleware to parse JSON
 app.use(express.json());
@@ -22,8 +62,37 @@ app.listen(port, () => {
 	console.log('Server listening to port', port)
 })
 
-
-// app.get('/start-engine', engine)
 app.get('/', (req, res) => {
 	res.send('HELLLOo')
+})
+
+app.post('/add-item', (req, res) => {
+	console.log("add-item endpoint")
+
+	eBay.trading.AddItem(itemData)
+	.then(result => {
+		const data = JSON.stringify(result, null, 2)
+		console.log(JSON.stringify(result, null, 2));
+		res.send({ok:true, data: data})
+	}).catch(e => {
+		console.error('add-item error: ', e);
+		res.send({ok:false, err: e.meta.Errors})
+	});
+
+	// eBay.ebayApiPostXmlRequest({
+	// 	serviceName: 'Trading',
+	// 	opType: 'AddItem',
+	// 	devId: eBayConfig.devId,
+	// 	certId: eBayConfig.certId,
+	// 	appName: eBayConfig.appName,
+	// 	authToken: eBayConfig.authToken,
+	// 	sandbox: eBayConfig.sandbox,
+	// 	params: itemData
+	//   }, (error, data) => {
+	// 	if (error) {
+	// 	  console.error('Error:', error);
+	// 	} else {
+	// 	  console.log('Response:', data);
+	// 	}
+	//   });	  
 })
